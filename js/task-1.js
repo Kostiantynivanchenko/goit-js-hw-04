01dd5a218f3a9648bd98ee4ccb7bb3b4a2a4ed3a
kostiantyn_ivanchenko <kokos2702@gmail.com>
2024-10-13 09:23:59 +0200

    first-task

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+  for (let key in products) {
+    totalQuantity += products[key];
+  }
+  return totalQuantity <= containerSize;
+}
